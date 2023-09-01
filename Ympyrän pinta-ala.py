3636aab6ba0d9ee4640c6681fc303d8484ce7bac
Zahraa0780 <Zahraaareef@gmail.com>
2023-09-01 19:46:27 +0300

    2nd commit

---
+++ Ympyrän pinta-ala.py
@@ -0,0 +1,8 @@
+# Pyydä käyttäjältä ympyrän säde
+sade = float(input("Anna ympyrän säde: "))
+
+# Laske ympyrän pinta-ala
+pinta_ala = 3.14159 * sade**2
+
+# Tulosta pinta-ala
+print("Ympyrän pinta-ala on:", pinta_ala)
